33b86744b6aa491685973090dcd039162a8e6292
Darren Kulp <darren@kulp.ch>
2017-09-14 05:55:33 -0700

    Generalizing

--- unmake_dict.c
+++ unmake_dict.c
@@ -1,6 +1,8 @@
 #include "huffman.h"
 #include <stdio.h>
 
+typedef int action(char, bitstring, void *);
+
 static int print_bits(FILE *out, bitstring bits)
 {
     char str[65] = "0";
@@ -14,14 +16,16 @@ static int print_bits(FILE *out, bitstring bits)
     return fprintf(out, "%d'b%s", bits.len, str);
 }
 
-static void print_node(FILE *out, char byte, bitstring bits)
+static int print_node(char byte, bitstring bits, FILE *out)
 {
     fprintf(out, "0x%hhX = ", byte);
     print_bits(out, bits);
     fputc('\n', out);
+
+    return 0;
 }
 
-static int process(long offset, FILE *in, FILE *out, bitstring b)
+static int process(long offset, FILE *in, bitstring b, action act, void *data)
 {
     unsigned char arr[2];
     fseek(in, offset, SEEK_SET);
@@ -32,11 +36,11 @@ static int process(long offset, FILE *in, FILE *out, bitstring b)
         bitstring l = { .bits = (b.bits << 1) | 0, .len = b.len + 1 },
                   r = { .bits = (b.bits << 1) | 1, .len = b.len + 1 };
 
-        process(offset + arr[0], in, out, l);
-        process(offset + arr[1], in, out, r);
+        process(offset + arr[0], in, l, act, data);
+        process(offset + arr[1], in, r, act, data);
     } else {
         // leaf node
-        print_node(out, arr[0], b);
+        act(arr[0], b, data);
     }
 
     return 0;
@@ -49,6 +53,6 @@ int main()
 
     bitstring bits = { .len = 0 };
 
-    process(ftell(in), in, out, bits);
+    process(ftell(in), in, bits, (action*)print_node, out);
 }
 
